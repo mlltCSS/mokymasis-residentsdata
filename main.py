7b0ef66a0ff6a2e309203570aaa7a0b7a68b2415
Mindaugas Leonas <mindaugas.leonas12345@gmail.com>
2020-10-13 14:23:13 +0300

    commit

--- main.py
+++ main.py
@@ -2,5 +2,8 @@ import csv
 file = csv.reader(open('dataset.csv', newline=''), delimiter=',', quotechar='|')
 for row in file:
     a = row[1].split(' ')
-    if len(a[1]) >= 7:
+    if len(a) == 3 and len(a[1]) >= 7:
+        print(a[0], a[1], a[2])
+        set = True
+    if len(a[1]) >= 7 and set != True:
         print(a[0], a[1])
