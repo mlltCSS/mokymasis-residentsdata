f5677c84efd2be533c7cfc983dba7bddd66a3e8b
Mindaugas Leonas <mindaugas.leonas12345@gmail.com>
2020-10-13 14:15:33 +0300

    commit

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
 import csv
 file = csv.reader(open('dataset.csv', newline=''), delimiter=',', quotechar='|')
 for row in file:
-    print(row[1])
-    # "Martynas.Bagdonas"
-    # "Marilyn Stephenson MD"
     a = row[1].split(' ')
-    # ["Martynas","Bagdonas"]
-    # ["Marilyn", "Stephenson", "MD"]
-    b = a[1].split(...)
-    # "Bagdonas"
-    print(a)
-    b =
+    if len(a[1]) >= 7:
+        print(a[0], a[1])
