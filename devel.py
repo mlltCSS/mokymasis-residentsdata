dc715ccee4c69b7fec0281114cdf3e396cf6e687
mllt <59761330+mlltCSS@users.noreply.github.com>
2020-11-02 17:46:51 +0200

    Add files via upload

--- devel.py
+++ devel.py
@@ -1,8 +1,9 @@
 import csv
 
-file = csv.reader(open('dataset.csv', newline=''), delimiter=',', quotechar='|')
+file = csv.reader(open('dataset2.csv', newline=''), delimiter=',', quotechar='|')
 
 for line in file:
-    y = (line[1].split(','))[0].split(' ')
+    y = line[1].split(',')
+    y = y[0].split(' ')
     if len(y[1]) >= 7:
         print(" ".join(y))
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import csv
 file = csv.reader(open('dataset.csv', newline=''), delimiter=',', quotechar='|')
 
 for line in file:
-    y = (line[1].split(','))[0].split(' ')
+    y = line[1].split(',')
+    y = y[0].split(' ')
     if len(y[1]) >= 7:
         print(" ".join(y))
